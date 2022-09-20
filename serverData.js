2bce53827bf8062bb93c05b95bd052444f96f245
AGarciaNY <aldairgarcia360@gmail.com>
2022-09-20 11:55:45 -0400

    feat: made account function work

--- serverData.js
+++ serverData.js
@@ -91,4 +91,30 @@ let serverData = {
   }
 }
 
-export default serverData
+export default serverData;
+
+// import serverData from './serverData.js';
+
+// //console.log(serverData.getAccountData('Ag', 'cat1'))
+// let makeAccount = document.getElementById('makeAccount')
+// makeAccount.addEventListener('click', () => {
+//   serverData.creatAccount('test4', 'p', 'p')
+// });
+
+// document.getElementById('signout').addEventListener('click', () => {
+//   serverData.signOutNow()
+// });
+// document.getElementById('signIN').addEventListener('click', () => {
+//   serverData.getAccountData('test3', 'p')
+// });
+// document.getElementById('seedata').addEventListener('click', () => {
+//   console.log(serverData)
+// })
+
+// document.getElementById('getting').addEventListener('click', () => {
+//   serverData.getWords()
+// })
+
+// document.getElementById('addWords').addEventListener('click', () => {
+//   serverData.addNewWord('there')
+// })

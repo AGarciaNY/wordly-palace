5b028b26c486b48a19fb0fee6a11fea616e04138
Aneika11 <aneikananton@gmail.com>
2022-09-20 11:57:24 -0400

    got the dictionary to work

--- script.js
+++ script.js
@@ -12,11 +12,12 @@ window.addEventListener("DOMContentLoaded", () => {
     let url = "https://random-words-with-pronunciation.p.rapidapi.com/word?rapidapi-key=d90feb3493msh188349b87904e98p1e8634jsne0ad3411d51a"
         fetch("https://random-words-with-pronunciation.p.rapidapi.com/word?rapidapi-key=d90feb3493msh188349b87904e98p1e8634jsne0ad3411d51a")
         .then(res => res.json()).then(json => {
-            console.log(json)
+            // console.log(json)
             let word = `${json[0].word}`
             nameOfWord.innerText = `${json[0].word}`
             defination.innerText = `Definition: ${json[0].definition}`
              pronounceW.innerText = `Pronunciation: ${json[0].pronunciation}`
+             let word2 = `${json[0].word}`
     //Event listener for adding the speech
         audioBtn.addEventListener("click", () => {
             speaks = [
@@ -26,33 +27,39 @@ window.addEventListener("DOMContentLoaded", () => {
                 }
             ]
          const msg = new SpeechSynthesisUtterance();
-            msg.volume = 1; // 0 to 1
-            msg.rate = 1; // 0.1 to 10
-            msg.pitch = 1.5; // 0 to 2
+            msg.volume = 1; 
+            msg.rate = 1; 
+            msg.pitch = 1.5; 
             msg.text  = `${json[0].word}` ;
         speechSynthesis.speak(msg);
         })
         })
         function fetch20Times() {
             for(let i = 1; i <= 20; i++) {
-             fetch("https://random-word-api.herokuapp.com/word").then(res => res.json()).then(data => {
+             fetch("https://api.api-ninjas.com/v1/randomword/?key=puHp1RTi4iddWO3YYmNuXQ==lTdWygeqoi5P3jQz").then(res => res.json()).then(data => {
+                // console.log(data)
                 let randomW = document.createElement("button")
                 arrayOfW.appendChild(randomW)
-                randomW.innerText = `${data[0]}`
+                randomW.innerText = `${data.word}`
                 randomW.style.marginTop = `${Math.random() * 10}px`
                 randomW.style.marginLeft = `${Math.random() * 10}px`
-                let word = `${data[0]}`
+                let word = `${data.word}`
               // eventlistener for when the button is clicked
                 randomW.addEventListener("click", () => {
                   arrayOfW.innerText = "";
                   fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
                   .then(res => res.json()).then(data2 => {
                     console.log(data2)
-                     let name = document.createElement("p")
-                     let meaning = document.createElement("p")
-                     let example = document.createElement("p")
-                     showing.append(name,meaning,example)
-                     name.innerText = `Word: ${data2[0].word}`
+
+                   
+                    //  let name = document.createElement("p")
+                    //  let meaning = document.createElement("p")
+                    //  let example = document.createElement("p")
+                    //  showing.append(name,meaning,example)
+                    //  name.innerText = `Word: ${data2.word}`
+                    //  meaning.innerText = `Meaning: ${data2.results[0].definition} `
+                    //  example.innerText = `Pronunciation: ${data2.pronunciation.all}`
+                    
                   })
                 })
               });

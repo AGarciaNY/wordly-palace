cd5bba6120b8dda894084245fd4dc5d0311b6f6a
marcwayn8 <juniormarch48@gmail.com>
2022-09-20 13:49:42 -0400

    feat:navbar, footer, character, dialogbox, mood meter, mood event listeners

--- script.js
+++ script.js
@@ -1,4 +1,89 @@
+window.addEventListener("DOMContentLoaded", () => {
+    //dom Elements
+    let nameOfWord = document.getElementById("nameOfWord")
+    let defination = document.getElementById("defination")
+    let pronounceW = document.getElementById("pronounce")
+    let audioBtn = document.getElementById("audiobtn")
+    let arrayOfW = document.getElementById("arrayOfWords")
+    let showing = document.getElementById("discription")
+    let dialogBox=document.getElementById("dialog-box")
+    let img=document.getElementById("img")
 
+
+    //styling
+    img.style.display="none";
+    dialogBox.style.display="none";
+
+    
+
+
+    // fetch("https://random-word-api.herokuapp.com/All").then(res => res.json())
+    // .then(data => console.log(data[0]))
+    //varibles
+    let url = "https://random-words-with-pronunciation.p.rapidapi.com/word?rapidapi-key=d90feb3493msh188349b87904e98p1e8634jsne0ad3411d51a"
+        fetch("https://random-words-with-pronunciation.p.rapidapi.com/word?rapidapi-key=d90feb3493msh188349b87904e98p1e8634jsne0ad3411d51a")
+        .then(res => res.json()).then(json => {
+            // console.log(json)
+            let word = `${json[0].word}`
+            nameOfWord.innerText = `${json[0].word}`
+            defination.innerText = `Definition: ${json[0].definition}`
+             pronounceW.innerText = `Pronunciation: ${json[0].pronunciation}`
+             let word2 = `${json[0].word}`
+     
+    //Event listener for adding the speech
+        audioBtn.addEventListener("click", () => {
+            speaks = [
+                {
+                  "name": "Alex",
+                  "lang": "en-US"
+                }
+            ]
+         const msg = new SpeechSynthesisUtterance();
+            msg.volume = 1; 
+            msg.rate = 1; 
+            msg.pitch = 1.5; 
+            msg.text  = `${json[0].word}` ;
+        speechSynthesis.speak(msg);
+        })
+        })
+        function fetch20Times() {
+            for(let i = 1; i <= 20; i++) {
+             fetch("https://api.api-ninjas.com/v1/randomword/?key=puHp1RTi4iddWO3YYmNuXQ==lTdWygeqoi5P3jQz").then(res => res.json()).then(data => {
+                // console.log(data)
+                let randomW = document.createElement("button")
+                arrayOfW.appendChild(randomW)
+                randomW.innerText = `${data.word}`
+                randomW.style.marginTop = `${Math.random() * 10}px`
+                randomW.style.marginLeft = `${Math.random() * 10}px`
+                let word = `${data.word}`
+              // eventlistener for when the button is clicked
+                randomW.addEventListener("click", () => {
+                  arrayOfW.innerText = "";
+                  fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
+                  .then(res => res.json()).then(data2 => {
+                    console.log(data2)
+                    dialogBox.innerHTML=`You selected ${word}! Click Next!`
+                    img.style.display="block";
+                    dialogBox.style.display="block";
+
+                   
+                    //  let name = document.createElement("p")
+                    //  let meaning = document.createElement("p")
+                    //  let example = document.createElement("p")
+                    //  showing.append(name,meaning,example)
+                    //  name.innerText = `Word: ${data2.word}`
+                    //  meaning.innerText = `Meaning: ${data2.results[0].definition} `
+                    //  example.innerText = `Pronunciation: ${data2.pronunciation.all}`
+                    
+                  })
+                })
+              });
+            }
+          }
+          fetch20Times()
+    })  
+
+    //submit event listener
     let submit=document.getElementById("submit")
     let next=document.getElementById("next")
     let img=document.getElementById("img")
@@ -33,7 +118,7 @@ happy.addEventListener('click', async(e) => {
 audio.play();
 
 });
-
+//tired mood array with random songs
 tired.addEventListener('click', async(e) => {
     e.preventDefault();
     const data = await getMainData();
